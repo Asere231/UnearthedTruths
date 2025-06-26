fd020bc325f6331f58522fe91880ef5ff3dd0dc6
Asere231 <br867228@ucf.edu>
2025-06-26 08:44:00 -0500

    updated pom.xml

--- src/main/java/com/example/unearthedtruths/UnearthedTruthsApplication.java
+++ src/main/java/com/example/unearthedtruths/UnearthedTruthsApplication.java
@@ -1,5 +1,6 @@
 package com.example.unearthedtruths;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class UnearthedTruthsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UnearthedTruthsApplication.class, args);
+        Dotenv.load();
     }
 
 }
