a8ce66ad5352733ec0a4dba9e21777e447ffa6b5
Bryan Aneyro <br867228@ucf.edu>
2025-06-26 09:24:01 -0500

    Update UnearthedTruthsApplication.java

--- src/main/java/com/example/unearthedtruths/UnearthedTruthsApplication.java
+++ src/main/java/com/example/unearthedtruths/UnearthedTruthsApplication.java
@@ -9,7 +9,6 @@ public class UnearthedTruthsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UnearthedTruthsApplication.class, args);
-        Dotenv.load();
     }
 
 }
